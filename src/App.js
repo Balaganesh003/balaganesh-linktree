0190d61313c550395fcc79d212b0288aae24dbf2
Balaganesh003 <k.balaganesh26@gmail.com>
2022-11-10 02:39:26 +0530

    added the all the card elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './routes/home/home.component'
-// import LinkCard from './components/link-card/link-card.component'
 
 import './App.css'
 
--- src/components/link_card.component.jsx
+++ src/components/link_card.component.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
-import { FaTwitter } from 'react-icons/fa'
+import { Fragment } from 'react'
+import {
+    FaTwitter,
+    FaGithub,
+    FaInstagram,
+    FaRegEnvelope,
+    FaDiscord,
+} from 'react-icons/fa'
 import './link_card.styles.css'
 
 const LinkCard = () => {
     return (
-        <a href="https://twitter.com/balaganesh_003">
-            <div className="link-card">
-                <FaTwitter className="link-card-icon" />
-                <p className="link-card-title">Twitter</p>
-            </div>
-        </a>
+        <Fragment>
+            <a href="https://twitter.com/balaganesh_003">
+                <div className="link-card">
+                    <FaTwitter className="link-card-icon" />
+                    <p className="link-card-title">Twitter</p>
+                </div>
+            </a>
+            <a href="https://twitter.com/balaganesh_003">
+                <div className="link-card">
+                    <FaGithub className="link-card-icon" />
+                    <p className="link-card-title">Github</p>
+                </div>
+            </a>
+            <a href="https://twitter.com/balaganesh_003">
+                <div className="link-card">
+                    <FaInstagram className="link-card-icon" />
+                    <p className="link-card-title">Instagram</p>
+                </div>
+            </a>
+
+            <a href="https://twitter.com/balaganesh_003">
+                <div className="link-card">
+                    <FaDiscord className="link-card-icon" />
+                    <p className="link-card-title">Discord</p>
+                </div>
+            </a>
+
+            <a href="https://twitter.com/balaganesh_003">
+                <div className="link-card">
+                    <FaRegEnvelope className="link-card-icon" />
+                    <p className="link-card-title">Discord</p>
+                </div>
+            </a>
+        </Fragment>
     )
 }
 
