a9546863e0db5c5d3a5299268160ec0fcff8931f
Balaganesh003 <k.balaganesh26@gmail.com>
2022-11-11 19:31:01 +0530

    modified linktree-data

--- src/linktree-data.js
+++ src/linktree-data.js
@@ -5,29 +5,32 @@ const LINKTREE_DATA = [
         url: 'https://twitter.com/balaganesh_003',
     },
     {
-        id: 2,
-        name: 'Instagram',
-        url: 'https://www.instagram.com/__balaganesh/',
-    },
-    {
         id: 3,
         name: 'Github',
         url: 'https://github.com/Balaganesh003',
     },
+
     {
         id: 4,
         name: 'LinkedIn',
         url: 'https://www.linkedin.com/in/balaganesh-k-512a0b215/',
     },
     {
-        id: 6,
-        name: 'Email',
-        url: 'https://mailto:k.balaganesh26@gmail.com',
+        id: 2,
+        name: 'Instagram',
+        url: 'https://www.instagram.com/__balaganesh/',
     },
+
     {
         id: 5,
         name: 'Discord',
         url: 'https://dsc.bio/balaganesh',
+    },
+
+    {
+        id: 6,
+        name: 'Email',
+        url: 'https://mailto:k.balaganesh26@gmail.com',
     },
 ]
 
