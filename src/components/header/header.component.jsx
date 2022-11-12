35d9a864c4439c0ba7d6c1edfb64dea4e3f9bba1
Balaganesh003 <k.balaganesh26@gmail.com>
2022-11-12 16:30:47 +0530

    added typewriting effect in header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-
 import { client, urlFor } from '../../client.js'
+import Typewriter from 'typewriter-effect'
 
 import './header.styles.css'
 const Header = () => {
@@ -19,6 +19,28 @@ const Header = () => {
             />
             <p>{profile.name}</p>
             <p>{profile.bio}</p>
+            I'm a{' '}
+            <Typewriter
+                className="Typewriter"
+                options={{
+                    autoStart: true,
+                    loop: true,
+                    cursor: 'I',
+                }}
+                onInit={(typewriter) => {
+                    typewriter
+                        .typeString('frontend developer')
+                        .pauseFor(2500)
+                        .deleteAll(200)
+                        .typeString('Student')
+                        .pauseFor(2500)
+                        .deleteAll(200)
+                        .typeString('Aspring fullstack web developer')
+                        .pauseFor(2500)
+                        .deleteAll(200)
+                        .start()
+                }}
+            />
         </div>
     ))
 }
